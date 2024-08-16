8472387c9064e1fd47e245584f354f362645a99d
kimbeff <kimbeff@gmail.com>
2024-08-16 19:59:49 +0300

    1

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,8 @@ import Features from './components/Features';
 import HowItWorks from './components/HowItWorks';
 import Footer from './components/Footer';
 
-import * as Sentry from '@sentry/react';
+
+
 
 const App = () => {
   return (
@@ -22,4 +23,4 @@ const App = () => {
   )
 }
 
-export default Sentry.withProfiler(App);
+export default App;
